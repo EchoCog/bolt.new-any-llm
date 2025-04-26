f7de866d058acd260da169cb2e38b5d32b05d1d8
dtecho <echo@rzo.io>
2025-04-26 10:29:17 +0000

    feat: replace pre-commit hook with a new version and disable the old one

--- app/lib/persistence/index.ts
+++ app/lib/persistence/index.ts
@@ -1,7 +1,7 @@
 // Re-export everything except 'db' which we'll export explicitly
 export * from './useChatHistory';
 // Export db and other functions from db.ts explicitly to avoid naming conflict
-export { getAll, setMessages, getMessages, getMessagesById, getMessagesByUrlId, 
-         deleteById, getNextId, getUrlId, forkChat, duplicateChat, 
-         createChatFromMessages, updateChatDescription, db, 
+export { getAll, setMessages, getMessages, getMessagesById, getMessagesByUrlId,
+         deleteById, getNextId, getUrlId, forkChat, duplicateChat,
+         createChatFromMessages, updateChatDescription, db,
          initDatabase, saveFiles, getFiles, getAllFileSets, deleteFiles } from './db';
